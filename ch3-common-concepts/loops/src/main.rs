d2105811d91593576b5e7ca6c33ab9e0b15d0878
Matthew Miller <matthew.miller@appian.com>
2021-03-16 15:25:16 -0400

    Chapter 3: control flow

---
+++ ch3-common-concepts/loops/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+
+    for elem in a.iter() {
+        println!("The value is: {}", elem);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!");
+}
