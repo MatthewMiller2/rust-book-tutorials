2695b267040a568c51b166d20915cbcf3a67f8cd
Matthew Miller <matthew.miller@appian.com>
2021-03-25 17:29:54 -0400

    Chapter 5: Structs

---
+++ ch5-structs/rectangles/src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
+    println!("rect1 is {:?}", rect1);
+    println!("The area of the rectangle is {}", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    println!("rect1 can hold rect2: {}", rect1.can_hold(&rect2));
+    println!("rect1 can hold rect3: {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(5);
+    println!("Square created with Rectangle::square(..): {:?}", square);
+}
